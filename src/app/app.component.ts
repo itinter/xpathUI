d4f569a09f59564c4c24605cb2ac3690d031e6d2
tranxuantan <xuantan0510@gmail.com>
2017-10-14 18:45:41 +0700

    14/10/2017

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,22 +4,23 @@ import { Http, Response } from '@angular/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
 	constructor(private http:Http){}
-  html = '';
-  title = 'app';
-  getxpath(url:string){
-  	 var url1 =url.replace('http://','');
-  	 url1=url1+'.';
-     this.http.get('http://localhost:8080/xpath/getxpath/'+url1).subscribe(data => console.log(data));
-  }
-  savexpath(){
-  	 this.http.get('http://localhost:8080/xpath/save/').subscribe(data => console.log(data));
-  }
+  title = 'Welcome to XPATH Getting Application!';
+  // getxpath(url:string){
+  // 	 var url1 =url.replace('http://','');
+  // 	 url1=url1+'.';
+  //    this.http.get('http://localhost:8080/xpath/getxpath/'+url1).subscribe(data => console.log(data));
+  // }
+  // savexpath(){
+  // 	 this.http.get('http://localhost:8080/xpath/save/').subscribe(data => console.log(data));
+  // }
   getxpath2(url:string){
   	 var url1 =url.replace('http://','');
   	 url1=url1+'.';
-     this.http.get('http://localhost:8080/xpath/getxpath2/'+url1).subscribe(data => this.html=data.text());
+     //code lỗi(1) this.http.get('http://localhost:8080/xpath/getxpath2/'+url1).subscribe(data => this.html=data.text());
+     this.http.get('http://localhost:8080/xpath/getxpath2/'+url1).subscribe(data => document.getElementById("show").setAttribute("srcdoc",data.text()));
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpModule} from '@angular/http';
+import { NgDatepickerModule } from 'ng2-datepicker';
 
 
 import { AppComponent } from './app.component';
@@ -13,7 +14,8 @@ import { DisplayComponent } from './app.displayhtml';
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    NgDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
