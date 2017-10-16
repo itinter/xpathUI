ab777b80c1d46c879d88ab1d50347d0c5a7eacd7
tranxuantan <xuantan0510@gmail.com>
2017-10-16 11:05:26 +0700

    16/10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, RequestOptions } from '@angular/http';
+import {HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -19,8 +20,12 @@ export class AppComponent {
   // }
   getxpath2(url:string){
   	 var url1 =url.replace('http://','');
-  	 url1=url1+'.';
+     var url1 =url.replace('https://','');
+  	 url1=url1+'.';    
      //code lỗi(1) this.http.get('http://localhost:8080/xpath/getxpath2/'+url1).subscribe(data => this.html=data.text());
-     this.http.get('http://localhost:8080/xpath/getxpath2/'+url1).subscribe(data => document.getElementById("show").setAttribute("srcdoc",data.text()));
+     //this.http.get('http://localhost:8080/xpath/getxpath2/'+url1).subscribe(data => document.getElementById("show").setAttribute("srcdoc",data.text()));
+     let params: URLSearchParams = new URLSearchParams();
+     params.set('url', url);
+     this.http.post('http://localhost:8080/XPath/getxpath3',{url: url}).subscribe(data => console.log(data));
   }
 }
