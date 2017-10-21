f8817870393bcae2a8ca6eff56ac695e46c20bb8
tranxuantan <xuantan0510@gmail.com>
2017-10-21 21:07:08 +0700

    21/10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Http, Response,Headers,URLSearchParams, RequestOptions } from '@angular/http';
+import { Http, Response, Headers, URLSearchParams, RequestOptions } from '@angular/http';
+import {Observable} from "rxjs/Rx";
+import 'rxjs/add/operator/toPromise';
 import {HttpParams} from '@angular/common/http';
 
 @Component({
@@ -16,45 +18,67 @@ export class AppComponent {
     //    this.http.get('http://localhost:8080/xpath/getxpath/'+url1).subscribe(data => console.log(data));
     // }
     // savexpath(){
-    //   if(this.http.get('http://localhost:8080/xpath/save/')){
-    //       alert("lưu thành công")
-    //   }else alert("lưu không thành công");
-    // }
+      //   if(this.http.get('http://localhost:8080/xpath/save/')){
+        //       alert("lưu thành công")
+        //   }else alert("lưu không thành công");
+        // }
 
-    savexpath(){
-      this.http.get('http://localhost:8080/xpath/save/').subscribe(data=>document.getElementById("labelMessage").innerHTML=data.text());
-      document.getElementById("btnsavexpath").setAttribute("disabled","disabled");
-      document.getElementById("btnsavexpath").style.backgroundColor = "gray";    
-    }
+        savexpath(){
+          this.http.get('http://localhost:8080/xpath/save/').subscribe(data=>document.getElementById("labelMessage").innerHTML=data.text());
+          document.getElementById("btnsavexpath").setAttribute("disabled","disabled");
+          document.getElementById("btnsavexpath").style.backgroundColor = "gray";    
+        }
 
-    getxpath2(url:string){      
-      if(url.startsWith("http://") || url.startsWith("https://")){
-        document.getElementById("labelWanring").style.display="none";
-        let cpHeadears = new Headers({'Content-Type': 'application/json'});
-        let options = new RequestOptions({headers: cpHeadears});
-        //this.http.post('http://localhost:8080/xpath/getxpath4',url,options).subscribe(data => document.getElementById("show2").innerHTML=data.text());
-        this.http.post('http://localhost:8080/xpath/getxpath4',url,options).subscribe(data => document.getElementById("show").setAttribute("srcdoc",data.text()));
-        document.getElementById("show").style.border="ridge";
-        document.getElementById("btnsavexpath").removeAttribute("disabled");
-        document.getElementById("btnsavexpath").style.backgroundColor = "red";
-        document.getElementById("labelMessage").innerHTML="";
-      }else{
-        document.getElementById("labelWanring").style.display="block";
-        document.getElementById("show").style.border="none";
-        document.getElementById("show").setAttribute("srcdoc","");
-        document.getElementById("btnsavexpath").setAttribute("disabled","disabled");
-        document.getElementById("btnsavexpath").style.backgroundColor = "gray";
-        document.getElementById("labelMessage").innerHTML="";
+        async showxpath(url:string){      
+          if(url.startsWith("http://") || url.startsWith("https://")){
+              let html="";
+              await this.getxpath(url).then(value=>html=value.text() );
+              if(html!=""){
+                document.getElementById("show").setAttribute("srcdoc",html);
+                this.urlvalid();
+              }else {document.getElementById("show").setAttribute("srcdoc","<h1>Error 404: Not Found<h1>");
+                this.urlnotvalid();
+              }
+  
+            this.success();
+          }else{
+            this.fail();
+          }
+
+          // var url1 =url.replace('http://','');
+          // var url1 =url.replace('https://','');
+          // url1=url1+'.';    
+          //code lỗi(1) this.http.get('http://localhost:8080/xpath/getxpath2/'+url1).subscribe(data => this.html=data.text());
+          //this.http.get('http://localhost:8080/xpath/getxpath2/'+url1).subscribe(data => document.getElementById("show").setAttribute("srcdoc",data.text()));
+          // let params: URLSearchParams = new URLSearchParams();
+          // params.set('url', url);
+          // this.http.get('http://localhost:8080/xpath/getxpath3', {search: params}).subscribe(data => document.getElementById("show").setAttribute("srcdoc",data.text()));
+
+        }
+
+        getxpath(url:string){
+          let cpHeadears = new Headers({'Content-Type': 'application/json'});
+          let options = new RequestOptions({headers: cpHeadears});
+          return this.http.post('http://localhost:8080/xpath/getxpath4',url,options).toPromise();
+        }
+
+        fail(){
+          document.getElementById("labelWanring").style.display="block";
+          document.getElementById("show").style.border="none";
+          document.getElementById("show").setAttribute("srcdoc","");
+          document.getElementById("labelMessage").innerHTML="";
+        }
+        success(){
+          document.getElementById("labelWanring").style.display="none";
+          document.getElementById("show").style.border="ridge";
+          document.getElementById("labelMessage").innerHTML="";
+        }
+        urlnotvalid(){
+          document.getElementById("btnsavexpath").setAttribute("disabled","disabled");
+          document.getElementById("btnsavexpath").style.backgroundColor = "gray";
+        }
+        urlvalid(){
+          document.getElementById("btnsavexpath").removeAttribute("disabled");
+          document.getElementById("btnsavexpath").style.backgroundColor = "red";
+        }
       }
-      
-
-      // var url1 =url.replace('http://','');
-      // var url1 =url.replace('https://','');
-      // url1=url1+'.';    
-      //code lỗi(1) this.http.get('http://localhost:8080/xpath/getxpath2/'+url1).subscribe(data => this.html=data.text());
-      //this.http.get('http://localhost:8080/xpath/getxpath2/'+url1).subscribe(data => document.getElementById("show").setAttribute("srcdoc",data.text()));
-      // let params: URLSearchParams = new URLSearchParams();
-      // params.set('url', url);
-      // this.http.get('http://localhost:8080/xpath/getxpath3', {search: params}).subscribe(data => document.getElementById("show").setAttribute("srcdoc",data.text()));
-    }
-}
